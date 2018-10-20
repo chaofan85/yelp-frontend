d0b4da7cfb7fd1c453e42db5bf6911f5a6a2758e
Chao Fan <fanchao05@gmail.com>
2018-10-20 12:25:52 -0400

    setup environment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import $ from "jquery";
 
 class App extends Component {
+  componentDidMount() {
+    let API_KEY =
+      "qAprBH0u-bIsf-RR_YF6wPToU-Ic-Nc3_XFPGnVXq4thwe1N0U2Fk5WrCCXe9jariZ28mwTtpekVGI9pI8BnUOCjQlRYVTNFJKitoYUyHArfFqPMhvb6L5Rk-D7LW3Yx";
+    $.ajax({
+      url:
+        "https://enigmatic-depths-85235.herokuapp.com/https://api.yelp.com/v3/businesses/search",
+      headers: { Authorization: `Bearer ${API_KEY}` },
+      // jsonp: "callback",
+      dataType: "json",
+      data: {
+        location: "nyc"
+        // format: "json"
+      },
+      success: function(response) {
+        console.log(response); // server response
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log("jqXHR:");
+        console.log(jqXHR);
+        console.log("textStatus:");
+        console.log(textStatus);
+        console.log("errorThrown:");
+        console.log(errorThrown);
+      }
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>Hello</div>
       </div>
     );
   }
